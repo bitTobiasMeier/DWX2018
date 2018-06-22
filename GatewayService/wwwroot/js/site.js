16cd3136d765ae9b0b7923d34ba93250c484a6b9
Tobias Meier <Tobias.Meier@bridging-it.de>
2018-06-22 16:32:42 +0200

    Beispiel erweitert

--- GatewayService/wwwroot/js/site.js
+++ GatewayService/wwwroot/js/site.js
@@ -1,1 +1,35 @@
+function postToServer() {
+    $('#postResult').text('');
+    var mydata = $('#postData').val();
+    var url = $('#postAddress').val();
+    var result = $.ajax(url, {
+        data: mydata,
+        contentType: 'application/json',
+        type: 'POST',
+        success: function (msg) {
+            $('#postResult').css('color', 'green');
+            $('#postResult').text(JSON.stringify(msg));
+        },
+        error: function (err) {
+            $('#postResult').css('color', 'red');
+            $('#postResult').text(JSON.stringify(err));
+        }
+    });
+}
+function getFromServer() {
+    $('#getResult').text('');
+    var url = $('#getAddress').val();
+    var result = $.ajax(url, {
+        contentType: 'application/json',
+        type: 'GET',
+        success: function (msg) {
+            $('#getResult').css('color', 'green');
+            $('#getResult').text(JSON.stringify(msg));
+        },
+        error: function (err) {
+            $('#getResult').css('color', 'red');
+            $('#getResult').text(JSON.stringify(err));
+        }
+    });
+}
 //# sourceMappingURL=site.js.map
